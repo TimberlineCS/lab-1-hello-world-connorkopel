b353e54ff4b3245a133f93821ca229fe99af2bef
connorkopel <22connor.kopel@boiseschools.net>
2020-08-20 15:38:53 +0000

    I made the art

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+//Connor
+//ap cs A per. 2
+
+public class HelloWorld{
+  public static void main(String args[]){
+/*
+this is a picture of a heart
+*/
+
+    System.out.println("  *  *");
+    System.out.println("*   *  *");
+    System.out.println(" *    *");
+     System.out.println("   *");
+
+  }
+
+
+
+
+}
